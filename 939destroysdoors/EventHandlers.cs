fd255757a301af8d212561d858e73a0eda183f38
An4r3w <75725607+An4r3w@users.noreply.github.com>
2021-03-10 14:16:03 +0100

    Add files via upload

---
+++ 939destroysdoors/EventHandlers.cs
@@ -0,0 +1,27 @@
+﻿using Exiled.Events.EventArgs;
+using Exiled.API.Features;
+using Interactables.Interobjects.DoorUtils;
+
+namespace _939destroysdoors
+{
+    public class EventHandlers
+    {
+        public void OnDoorInteract(InteractingDoorEventArgs ev)
+        {
+            if(ev.Player.Role == RoleType.Scp93953)
+            {
+                var door = ev.Door as IDamageableDoor;
+
+                door.IsDestroyed = true;
+            }
+
+            else if(ev.Player.Role == RoleType.Scp93989)
+            {
+                var door = ev.Door as IDamageableDoor;
+
+                door.IsDestroyed = true;
+            }
+
+        }
+    }
+}
---
+++ 939destroysdoors/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Features;
+using System;
+using PlayerEvent = Exiled.Events.Handlers.Player;
+
+namespace _939destroysdoors
+{
+     public class Plugin : Plugin<Config>
+    {
+        public override string Author { get; } = "An4r3w";
+
+        public override string Name { get; } = "939destroysdoors";
+
+        public override string Prefix { get; } = "939destroysdoors";
+
+        public override Version RequiredExiledVersion { get; } = new Version(2, 0, 0);
+
+        public override Version Version { get; } = new Version(1, 0, 0);
+
+        internal EventHandlers EventHandlers { get; set; }
+
+
+        public override void OnEnabled()
+        {
+            this.EventHandlers = new EventHandlers();
+
+            PlayerEvent.InteractingDoor += EventHandlers.OnDoorInteract;
+        }
+
+        public override void OnDisabled()
+        {
+            this.EventHandlers = null;
+        }
+
+    }
+}
